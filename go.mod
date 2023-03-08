89d756ba3a84bea4c97d85b01ab62affe4ffd10d
Linuxea <Linuxea@foxmail.com>
2023-03-09 01:13:51 +0800

    no proxy

--- go.mod
+++ go.mod
@@ -2,9 +2,4 @@ module linuxea.com/proxy
 
 go 1.18
 
-require (
-	github.com/elazarl/goproxy v0.0.0-20220417044921-416226498f94
-	github.com/magisterquis/connectproxy v0.0.0-20200725203833-3582e84f0c9b
-)
-
-require golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
+require github.com/elazarl/goproxy v0.0.0-20220417044921-416226498f94
--- main.go
+++ main.go
@@ -1,66 +1,25 @@
 package main
 
 import (
+	_ "embed"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"net/url"
-	"strings"
-
-	_ "embed"
 
 	"github.com/elazarl/goproxy"
-	"github.com/magisterquis/connectproxy"
-	"golang.org/x/net/proxy"
 )
-
-//go:embed white
-var whites string
 
 func main() {
 
-	do := doProxy()
-	no := noProxy()
-
-	whiteList := strings.Split(whites, ",")
-	for _, v := range whiteList {
-		fmt.Println(v)
-	}
-
-	log.Fatal(http.ListenAndServe(":8888", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		for index := range whiteList {
-			if whiteList[index] != "" && strings.Contains(r.Host, whiteList[index]) {
-				fmt.Println(r.Host + "不走代理")
-				no.ServeHTTP(w, r)
-				return
-			}
-		}
-
-		fmt.Println(r.Host + "走代理")
-		do.ServeHTTP(w, r)
-
-	})))
-
-}
-
-func doProxy() http.Handler {
-
-	hs := goproxy.NewProxyHttpServer()
-
-	u, _ := url.Parse("http://127.0.0.1:50475")
-	d, _ := connectproxy.New(u, proxy.Direct)
-	hs.Tr = &http.Transport{
-		Dial: d.Dial,
-	}
-	hs.Verbose = true
-
-	return hs
-}
-
-func noProxy() http.Handler {
+	port := flag.String("port", "8888", "port")
+	flag.Parse()
+	fmt.Println("port is", *port)
 
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = true
-	return proxy
+	log.Fatal(http.ListenAndServe(":"+*port, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
+	})))
+
 }
